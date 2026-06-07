0738773f0b03cb5042f5ac79e7a8567f511979f4
lycoris-red-spider <isnasetiawidiati@gmail.com>
2026-06-07 18:51:45 +0700

    Update welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     function goLogin() {
-        window.location.href = "login/index.html";
+        window.location.href = "Login.html";
     }
 
     function logout() {
